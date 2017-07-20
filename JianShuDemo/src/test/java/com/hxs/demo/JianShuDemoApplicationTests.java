06b355f9477b74b6e0819db18c420f6fcdf978d5
xiaohan <623365749@qq.com>
2017-07-20 17:53:51 +0800

    init

---
+++ JianShuDemo/src/test/java/com/hxs/demo/JianShuDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hxs.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JianShuDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
